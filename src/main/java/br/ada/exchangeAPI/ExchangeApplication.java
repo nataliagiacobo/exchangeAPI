36244e83e9e520c7961ba169f947b8b40d65f2a4
Natalia Giacobo Rohr <natalia.giacobo@gmail.com>
2023-10-18 08:50:53 -0300

    Criado projeto base ExchangeAPI e conexão com banco de dados

---
+++ src/main/java/br/ada/exchangeAPI/ExchangeApplication.java
@@ -0,0 +1,11 @@
+package br.ada.exchangeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ExchangeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeApplication.class, args);
+	}
+}
---
+++ src/test/java/br/ada/exchangeAPI/ExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ada.exchangeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
