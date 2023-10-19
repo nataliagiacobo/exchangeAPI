bb6caa5e9e44582919596c49405546c0e3bc4793
Raquel Carvalho <raquel.cpc90@gmail.com>
2023-10-19 16:09:16 -0300

    feat:adicionado endpoint de consulta do cliente pelo Cpf_controller-service-repository

--- src/main/java/br/ada/exchangeAPI/repository/CustomerRepository.java
+++ src/main/java/br/ada/exchangeAPI/repository/CustomerRepository.java
@@ -7,18 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-//    IMPLEMENTAR NA CUSTOMERSERVICE
-//    @Override
-//    @Query(value = "SELECT * FORM CUSTOMERS WHERE ACTIVE = TRUE", nativeQuery = true)
-//    Page<Customer> findAll (Pageable pageable);
-
-
-
-//    List<Customer> findAllByName(String name);
-
-//    @Query(value = "SELECT * FROM CUSTOMERS WHERE CPF = :customerCpf", nativeQuery = true)
-//    Customer findCustomerByCpf(String customerCpf);
-
-
+    @Query(value = "SELECT * FROM CUSTOMERS WHERE CPF = :customerCpf", nativeQuery = true)
+    Customer findCustomerByCpf(String customerCpf);
 
 }
