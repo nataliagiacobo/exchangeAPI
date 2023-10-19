ed207e6f03e847ef170f80f283f550ac18b26117
karenCLima <lima.kcls@gmail.com>
2023-10-19 09:46:43 -0300

    feat: adicionando dependencias

--- src/main/java/br/ada/exchangeAPI/service/CustomerService.java
+++ src/main/java/br/ada/exchangeAPI/service/CustomerService.java
@@ -1,5 +1,17 @@
 package br.ada.exchangeAPI.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ada.exchangeAPI.controller.dto.CustomerResponse;
+import br.ada.exchangeAPI.repository.CustomerRepository;
+
+@Service
 public class CustomerService {
+	
+	@Autowired
+	CustomerRepository customerRepository;
 
 }
