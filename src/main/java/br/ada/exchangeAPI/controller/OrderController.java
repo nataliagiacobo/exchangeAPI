aab5057a8b36a5713c2748bbf966288d4d5592bb
tdthais <thais.tdourado@gmail.com>
2023-10-19 17:45:44 -0300

    Criada a OrderController

---
+++ src/main/java/br/ada/exchangeAPI/controller/OrderController.java
@@ -0,0 +1,14 @@
+package br.ada.exchangeAPI.controller;
+
+import br.ada.exchangeAPI.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+}
